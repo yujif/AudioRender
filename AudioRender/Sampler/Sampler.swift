53f8c6ca4cabb8cceda6dc0d4267f27e97b4dfac
Yuji Fujisaka <yuji-fujisaka@cookpad.com>
2019-03-23 12:06:51 +0900

    Accelerate merge

--- AudioRender/Sampler/Sampler.swift
+++ AudioRender/Sampler/Sampler.swift
@@ -23,8 +23,8 @@ let strategy:DsStrategy = .maxValue
 //
 // MARK: - Accelerate Framework selection
 //
-let useAccelForMerge = false
 let useAccelForDs = true
+let useAccelForMerge = true
 let useAccelForPeakCalc = false
 let useAccelForBuildPoints = false
 
@@ -362,6 +362,8 @@ class Sampler: NSObject {
                     //
                     // Insert Accelerate merge code here
                     //
+                    var avg: Float = 0.5
+                    vDSP_vasm(fcd[0], 1, fcd[1], 1, &avg, sfd, 1, vDSP_Length(frameLength))
                 }
             }
             sampleBuffer.frameLength = frameLength
